cf4183b9977be5b7990db48c41bed71165e50300
Sras2898 <srashti.b2020@gmail.com>
2025-06-03 22:37:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-constbutton = document.getElementById('search-button');
-const input = document.getElementById('city-input');
+let screen = document.getElementById('screen');
+buttons =  document.querySelector('button');
+let screenValue = ' ';
+for(item of button){
+          item.addEventListener('click')  ,  (e)=>{
+               buttonText = e . target.innerText;
+               console.log('button text is ' ,  buttonText);
+               if(buttonText == 'X'  )  {
+                      buttonText= '*';
+                      screen.value += buttonText;
+                      screen.value = screenValue;
+               }
+               else if (buttonText = '=' )  {
+                     screen.value= screenValue;
+                }
+                 else if (buttonText == '=' ){
+                        screen.value = eval(screenValue);
+              }
+              else {
+                  screenValue+= buttonText;
+                  screen.value= screenValue;
+              }
 
-const cityName = document.getElementById('city-name');
-const cityTime = document.getElementById('city-time');
-const cityTemp = document.getElementById('city-temp');
-
-async function getData(cityName)   {
-        const promise = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=1b8a10ef3788661163ac6c50013c6ca
-&q=${cityName}`
-
-       ) ;
-        return await promise.json();
-}
-button.addEventListener('click', async () => {
-    const value = input.value;
-    getData result = await getData(value);
-    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
-    cityTime.innerText = result.location.localtime;
-    cityTemp.innerText = result.current.temp_c;
-}); 
+          } 
+  }s
